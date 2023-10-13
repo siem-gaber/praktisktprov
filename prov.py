0c43c2472bcd01bafe1da7a1ec9aae48ef6c9ff5
siem-gaber <116160960+siem-gaber@users.noreply.github.com>
2023-10-13 10:45:46 +0200

    gick_at_helvetet

---
+++ prov.py
@@ -0,0 +1,20 @@
+print("Ei22 - praktiskt prov ht23")
+
+Rtot = int(input("Hur många resistorer har du ? "))
+
+print("Ange resistorernas värden:")
+
+resistorer = []
+for i in range(Rtot):
+    resistor = int(input(f"R{i + 1} = "))
+    resistorer.append(resistor)
+
+if any(val == 0 for val in resistorer):
+    Serieresistans = 0
+    Parallellresistans = 0
+else:
+    Serieresistans = sum(resistorer)
+    Parallellresistans = 1 / sum(1 / val for val in resistorer)
+
+print("Serieresistans:", Serieresistans)
+print("Parallellresistans:", Parallellresistans)
